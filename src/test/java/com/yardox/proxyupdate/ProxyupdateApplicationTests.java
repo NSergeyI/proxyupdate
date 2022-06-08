9830fad180b7f405bfadad5d656348f8ac361689
SergeyN <yardox@gmail.com>
2022-06-08 11:35:27 +0400

    add test mvc and datasource

--- src/test/java/com/yardox/proxyupdate/ProxyupdateApplicationTests.java
+++ src/test/java/com/yardox/proxyupdate/ProxyupdateApplicationTests.java
@@ -1,13 +1,50 @@
 package com.yardox.proxyupdate;
 
+import com.yardox.proxyupdate.model.MyProxy;
+import com.yardox.proxyupdate.service.IMyProxyService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProxyupdateApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private IMyProxyService myProxyService;
+
+    @Test
+    public void rootShouldReturnIndexHtml() throws Exception {
+        ResultActions test = this.mockMvc.perform(get("/"));
+        this.mockMvc.perform(get("/"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(view().name("forward:index.html"));
+    }
+
+    @Test
+    public void showProxiesShouldReturnTableProxies() throws Exception {
+
+        this.mockMvc.perform(get("/showProxies"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("List proxy")));
+    }
+
+    @Test
+    public void dataSourceReturnData() throws Exception {
+        List<MyProxy> myProxies = (List<MyProxy>) myProxyService.findAll();
+        assertThat(myProxies).isNotEmpty();
+    }
 
 }
